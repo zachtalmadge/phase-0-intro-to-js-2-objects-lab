8991536f340324a28bc5def96a0464dbf67c4fbb
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-08-09 16:54:13 -0400

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+let employee = {
+    name: "Zach",
+    streetAddress: "180 smith street"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newObject = {...employee, [key]: value};
+    return newObject;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee
+
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let newObject = {...employee};
+    delete newObject[key];
+    return newObject
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee
+
+}
